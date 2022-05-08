e91f9561a1c8a6b9e60d54c6b19189b7a00eec62
jebarsan <81435464+jebarsan@users.noreply.github.com>
2022-05-08 21:31:04 +0530

    commit to main branch

---
+++ resources/views/users/admin/dashboard.blade.php
@@ -0,0 +1,34 @@
+@extends('users.admin.layouts.master')
+@section('content')
+    <!-- begin:: Content -->
+    <div class="kt-content  kt-grid__item kt-grid__item--fluid kt-grid kt-grid--hor" id="kt_content">
+        <!-- begin:: Content Head -->
+        <div class="kt-subheader  kt-grid__item" id="kt_subheader">
+            <div class="kt-container  kt-container--fluid ">
+                <div class="kt-subheader__main">
+
+                    <h3 class="kt-subheader__title">Well come to HMS Hospitals</h3>
+                    
+
+                    <span class="kt-subheader__separator kt-subheader__separator--v"></span>
+
+                    <span class="kt-subheader__desc"></span>
+
+                   
+
+                    <div class="kt-input-icon kt-input-icon--right kt-subheader__search kt-hidden">
+                        <input type="text" class="form-control" placeholder="Search order..." id="generalSearch">
+                        <span class="kt-input-icon__icon kt-input-icon__icon--right">
+                        <span><i class="flaticon2-search-1"></i></span>
+                        
+                </span>
+                    </div>
+                </div>
+                
+        <!-- end:: Content Head -->
+        
+        
+    </div>
+    
+    <!-- begin:: Content -->
+@endsection
